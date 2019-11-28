0af33de9872672e8cbd7f9c482f94f441c490539
Lukasha707 <43203369+Lukasha707@users.noreply.github.com>
2019-11-28 13:38:48 +0100

    Prueba de velocidad del actor

---
+++ Assets/Scripts/PruebaVelocidadSim.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PruebaVelocidadSim : MonoBehaviour
+{
+    public enum loopType
+    {
+        update, fixedUpdate
+    }
+
+    public loopType tipoDeLoop;
+    [Range(1,100)]
+    public float timeSpeed = 1;
+    public Vector3 velocidad = new Vector3(0, 0, 5);
+    public Vector3 anguloRot = new Vector3(0, 20, 0);
+
+    private void Start()
+    {
+        if (tipoDeLoop == loopType.fixedUpdate)
+        {
+            Time.timeScale = timeSpeed;
+        }
+    }
+
+    void Update()
+    {
+        if(tipoDeLoop == loopType.update)
+        {
+            Time.timeScale = timeSpeed;
+            moverse();
+        }
+    }
+    void FixedUpdate()
+    {
+        if (tipoDeLoop == loopType.fixedUpdate)
+        {
+            Time.timeScale = timeSpeed;
+            moverse();
+        }
+    }
+
+    public void moverse()
+    {
+        this.transform.Rotate(anguloRot);
+        this.transform.Translate(velocidad);
+    }
+}
