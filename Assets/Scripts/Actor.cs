d93e1ae807a14e890a72e5a40c85f822ab33ed38
Irene <43203578+wolveneri@users.noreply.github.com>
2019-11-29 18:53:33 +0100

    color tex + ini variables + spawn
    -Inicializar variables al hacer aparecer a los actores
    -Spawnear los actores en el mapa
    -Posibilidad de cambiar color tex para las especies
    
    Co-Authored-By: lukasha707 <lukasha707@users.noreply.github.com>

--- Assets/Scripts/Actor.cs
+++ Assets/Scripts/Actor.cs
@@ -5,42 +5,77 @@ using UnityEngine;
 public class Actor : MonoBehaviour
 {
     #region variables
-    private float tamano;
-    private float velocidad;
-    private float rangoSens;
-    private bool soyHembra;
-    private bool embarazada;
-    private int tGestacion;
-    private int nHijos;
-    private float fuerzaBase;
-    private float atractivoBase;
-    private float agroMismoSexBase;
-    private float agroDistSexBase;
-    private float agroOtrosBase;
-    private float miedo;
-    private float sociabBase;
-    private bool sociable;
-    private bool enManada;
-    private float vulneBase;
-    private int duracionSueño;
-    private int vEnvejecimiento;
-    private float pelaje;
-    private float edad;
+    [System.Serializable]
+    public struct Textura
+    {
+        public string nombre;
+        public Texture tex;
+    }
 
-    private float vida;
-    private float energia;
-    private float agua;
-    private float reproduccion;
-    private float sueño;
+    public Textura[] texturas;
 
-    private float fuerzaFinal;
-    private float atractivoFinal;
-    private float agroMismoSexo;
-    private float agroDistSexo;
-    private float agroOtros;
-    private float sociabFinal;
-    private float variacionTempIdeal;
-    private float vulneFinal;
+    [Header("Variables Actor")]
+    [Space(5)]
+    public float tamano;
+    public float velocidad;
+    public float rangoSens;
+    public bool soyHembra;
+    public bool embarazada;
+    public int tGestacion;
+    public int nHijos;
+    public float fuerzaBase;
+    public float atractivoBase;
+    public float agroMismoSexBase;
+    public float agroDistSexBase;
+    public float agroOtrosBase;
+    public float miedo;
+    public float sociabBase;
+    public bool sociable;
+    public bool enManada;
+    public float vulneBase;
+    public int duracionSueño;
+    public int vEnvejecimiento;
+    public float pelaje;
+    public float edad;
+
+    [Header("Variables segun edad")]
+    [Space(5)]
+    //variables que dependen de la edad
+    public float tamanoEdad;
+    public float velocidadEdad;
+    public float rangoSensEdad;
+    public float fuerzaBaseEdad;
+    public float atractivoBaseEdad;
+    public float vulneBaseEdad;
+    public float pelajeEdad;
+
+    [Header("Variables de necesidad")]
+    [Space(5)]
+    public float vida;
+    public float energia;
+    public float agua;
+    public float reproduccion;
+    public float sueño;
+
+    [Header("Variables ambientales")]
+    [Space(5)]
+    public float temperatura;
+    public float humedad;
+
+    [Header("Variables finales")]
+    [Space(5)]
+    public float fuerzaFinal;
+    public float atractivoFinal;
+    public float agroMismoSexo;
+    public float agroDistSexo;
+    public float agroOtros;
+    public float sociabFinal;
+    public float variacionTempIdeal;
+    public float vulneFinal;
+
+    [Header("Objetivo")]
+    [Space(5)]
+    public Vector3 objetivo;
 
     private GameManager manager;
     #endregion
@@ -67,22 +102,121 @@ public class Actor : MonoBehaviour
     }
 
     //animaciones
-    private void LateUpdate()
+    void LateUpdate()
     {
         
     }
 
-    #region Maquina de estados
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(this.transform.position, rangoSensEdad);
+    }
+
+#region Maquina de estados
     private void tomaDecisiones()
     {
-
+        this.transform.Rotate(0, 2, 0);
+        this.transform.Translate(0, 0, velocidadEdad);
     }
-    #endregion
+#endregion
 
-    #region metodos interaccion
-    private void nacer()
+#region metodos interaccion
+    public void nacer()
     {
+        /*tamano;
+        velocidad;
+        rangoSens;
+        soyHembra;
+        embarazada;
+        tGestacion;
+        nHijos;
+        fuerzaBase;
+        atractivoBase;
+        agroMismoSexBase;
+        agroDistSexBase;
+        agroOtrosBase;
+        miedo;
+        sociabBase;
+        sociable;
+        enManada;
+        vulneBase;
+        duracionSueño;
+        vEnvejecimiento;
+        pelaje;
+        edad;
 
+        vida;
+        energia;
+        agua;
+        reproduccion;
+        sueño;
+
+        fuerzaFinal;
+        atractivoFinal;
+        agroMismoSexo;
+        agroDistSexo;
+        agroOtros;
+        sociabFinal;
+        variacionTempIdeal;
+        vulneFinal;*/
     }
-    #endregion 
+
+    public void aparecer()
+    {
+        manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        //variables actor
+        tamano = manager.tamanoEstandar;
+        velocidad = manager.velocidadEstandar;
+        rangoSens = manager.rangoSensEstandar;
+        soyHembra = Random.Range(0f, 1f) < manager.proporcionHembras ? true : false;
+        embarazada = false;
+        tGestacion = manager.tGestacionEstandar;
+        nHijos = manager.nHijosEstandar;
+        fuerzaBase = manager.fuerzaBaseEstandar;
+        atractivoBase = manager.atractivoBaseEstandar;
+        agroMismoSexBase = manager.agroMismoSexEstandar;
+        agroDistSexBase = manager.agroDistSexEstandar;
+        agroOtrosBase = manager.agroOtrosEstandar;
+        miedo = manager.miedoEstandar;
+        sociabBase = manager.sociabBaseEstandar;
+        sociable = false;
+        enManada = false;
+        vulneBase = manager.vulneBaseEstandar;
+        duracionSueño = manager.duracionSueñoEstandar;
+        vEnvejecimiento = manager.vEnvejecimientoEstandar;
+        pelaje = manager.pelajeEstandar;
+        edad = manager.edadActoresIniciales;
+
+        tamanoEdad = tamano * Mathf.Sin(Mathf.Deg2Rad * edad);
+        velocidadEdad = velocidad * Mathf.Sin(Mathf.Deg2Rad * edad);
+        rangoSensEdad = rangoSens * Mathf.Sin(Mathf.Deg2Rad * edad);
+        fuerzaBaseEdad = fuerzaBase * Mathf.Sin(Mathf.Deg2Rad * edad);
+        atractivoBaseEdad = atractivoBase * Mathf.Sin(Mathf.Deg2Rad * edad);
+        vulneBaseEdad = Mathf.Max(Mathf.Abs(vulneBase * Mathf.Cos(Mathf.Deg2Rad * edad)), vulneBase * 0.1f);
+        pelajeEdad = pelaje * Mathf.Sin(Mathf.Deg2Rad * edad);
+
+        vida = 100;
+        energia = 100;
+        agua = 100 + tamanoEdad * 10;
+        reproduccion = 100;
+        sueño = 100;
+
+        temperatura = 0;
+        humedad = 0;
+
+        fuerzaFinal = fuerzaBaseEdad + tamanoEdad;
+        atractivoFinal = atractivoBaseEdad + fuerzaFinal + pelajeEdad;
+        agroMismoSexo = agroMismoSexBase * (1 - Mathf.Min(energia/100, reproduccion/100));
+        agroDistSexo = Mathf.Max(agroDistSexBase * ((1 - energia/100) - (1 - reproduccion/100)), 0);
+        agroOtros = Mathf.Max(agroOtrosBase - miedo);
+        sociabFinal = Mathf.Max(sociabBase + miedo/10, 0);
+        variacionTempIdeal = Mathf.Abs(temperatura - (1 - pelajeEdad));
+        vulneFinal = Mathf.Min(vulneBaseEdad + (variacionTempIdeal * 0.1f), 1);
+
+        //color actor
+        this.transform.GetChild(0).GetComponent<Renderer>().material.SetColor("_Color", manager.colorIni);
+        this.transform.GetChild(1).GetChild(0).GetComponent<Renderer>().material.SetColor("_Color", manager.colorIni);
+    }
+#endregion
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,7 +8,23 @@ public class GameManager : MonoBehaviour
     [Space(5)]
     [Min(1)]
     public int numActoresIniciales = 10;
+    public float edadActoresIniciales = 10;
+    public float radioSpawn = 10;
+    public float alturaSpawn = 5;
+    public enum BocaType
+    {
+        Herviboro, Omnivoro, Carnivoro
+    }
+    public enum ColorType
+    {
+        Cyan, Magenta, Amarillo, Rojo, Verde, Azul, Blanco
+    }
+
     [Header("Variables Estandar")]
+    public BocaType bocaTipoEstandar = BocaType.Herviboro;
+    public ColorType colorEstandar = ColorType.Rojo;
+    [HideInInspector]
+    public Color colorIni;
     [Min(0.001f)]
     [Tooltip("Esto edita el tamaño")]
     public float tamanoEstandar = 1f;
@@ -16,6 +32,8 @@ public class GameManager : MonoBehaviour
     public float velocidadEstandar = 5f;
     [Min(0.001f)]
     public float rangoSensEstandar = 5f;
+    [Range(0, 1)]
+    public float proporcionHembras = 0.5f;
     [Min(1)]
     public int tGestacionEstandar = 5;
     [Min(0)]
@@ -47,6 +65,7 @@ public class GameManager : MonoBehaviour
     {
         update, fixedUpdate
     }
+
     [Header("Variables de control de Simulacion")]
     [Space(5)]
     public LoopType tipoLoop = LoopType.update;
@@ -62,11 +81,32 @@ public class GameManager : MonoBehaviour
     [Min(1f)]
     public float maxVariabilidad = 15;
 
+    [Header("Prefabs")]
+    [Space(5)]
+    public GameObject herviboro;
+    public GameObject omnivoro;
+    public GameObject carnivoro;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        colorIni = colorEstandar == ColorType.Cyan ? Color.cyan : (
+            colorEstandar == ColorType.Magenta ? Color.magenta : (
+            colorEstandar == ColorType.Amarillo ? Color.yellow : (
+            colorEstandar == ColorType.Azul ? Color.blue : (
+            colorEstandar == ColorType.Rojo ? Color.red : (
+            colorEstandar == ColorType.Verde ? Color.green : Color.white)))));
+
+        for (int i = 0; i<numActoresIniciales; i++)
+        {
+            GameObject actor = Instantiate(bocaTipoEstandar == BocaType.Herviboro ? herviboro : (
+                bocaTipoEstandar == BocaType.Carnivoro ? carnivoro : omnivoro),
+                new Vector3 (this.transform.position.x + Random.Range(-radioSpawn, radioSpawn), alturaSpawn, this.transform.position.z + Random.Range(-radioSpawn, radioSpawn)),
+                Quaternion.identity);
+            actor.transform.SetParent(GameObject.FindGameObjectWithTag("BichitosGroup").transform);
+            actor.GetComponent<Actor>().aparecer();
+        }
     }
 
     // Update is called once per frame
